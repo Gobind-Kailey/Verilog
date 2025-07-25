f5b2173aba8dc449570cd19b8ff4377ba28e331b
kaileg2 <gobind2975@gmail.com>
2025-07-25 00:26:03 -0400

    Pushing updates

--- assignments.v
+++ assignments.v
@@ -1,13 +1,12 @@
-module Majority (A, B, C, Y);                 	
-   input  A, B, C;			
-   output Y; 
+module Majority (A, B, C, Y);
+   input  A, B, C;
+   output Y;
+   reg Y;
 
-   reg Y;           		
-                   	          	
-// student code here
-   always @(*)
-	begin 
-		if((A & B == 1'b1) || (A & C == 1'b1) || (C & B == 1'b1)) assign Y = 1'b1;
-		else assign Y = 1'b1; 
-	end
-endmodule // Majority  
+   always @(*) begin
+      if ((A & B) | (A & C) | (B & C))
+         Y = 1'b1;
+      else
+         Y = 1'b0;
+   end
+endmodule
---
+++ practice_4_bit_adder.v
@@ -0,0 +1,36 @@
+
+module FullAdd4( A,B,Cin,Sum,Cout);                	
+    input [3:0] A, B;, 
+    input Cin; 			
+    output [3:0] Sum;
+    output Cout;
+
+                   	          	
+// student code here
+    // genvar i; // Declare a variable for generating instances 
+    //     // Note that genvar can be used to create an index for a loop
+    // generate
+    //     for(i = 0; i<4; i= i+1) begin : full_adder_loop
+    
+    
+    
+    // endgenerate
+    
+    wire [0:3] carry; // Declare a wire to hold the carry bit 
+
+    assign Sum[0] = A[0] ^ B[0] ^ Cin; // Calculate the sum for the least significant bit
+    assign carry[0] = (A[0] & B[0]) | (A[0] & Cin) | (B[0] & Cin); // Calculate the carry for the least significant bit
+
+    assign Sum[1] = A[1] ^ B[1] ^ carry[0]; // Calculate the sum for the second bit
+    assign carry[1] = (A[1] & B[1]) | (A[1] & carry[0]) | (B[1] & carry[0]); // Calculate the carry for the second bit      
+
+    assign Sum[2] = A[2] ^ B[2] ^ carry[1]; // Calculate the sum for the third bit
+    assign carry[2] = (A[2] & B[2]) | (A[2] & carry[1]) | (B[2] & carry[1]); // Calculate the carry for the third bit
+
+    assign Sum[3] = A[3] ^ B[3] ^ carry[2]; // Calculate the sum for the most significant bit
+    assign Cout = (A[3] & B[3]) | (A[3] & carry[2]) | (B[3] & carry[2]); // Calculate the final carry out
+
+    
+
+
+endmodule // Majority  
