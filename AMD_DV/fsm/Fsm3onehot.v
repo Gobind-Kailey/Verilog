bad3e22facfb3203dba64c008571c1f11c20c30b
kaileg2 <gobind2975@gmail.com>
2025-11-03 09:47:24 -0500

    FSM one hot

--- AMD_DV/fsm/Fsm3onehot.v
+++ AMD_DV/fsm/Fsm3onehot.v
@@ -11,14 +11,23 @@
 
 module top_module
 (
-    input wire[1:0] state,
-    input 
+    input wire[3:0] state,
     input wire in,
-    output reg q, 
-
-
-
+    output reg out, 
+    output reg[3:0] next_state
 );
+
+// parameter A = 4'b0001, B = 4'b0010, C = 4'b0100, D = 4'b1000; 
+
+
+
+    assign next_state[0] = (~in) & (state[0] | state[2]);   
+    assign next_state[1] = (in) & (state[0] | state[1] | state[3]);
+    assign next_state[2] = (~in) & (state[1] | state[3]); 
+    assign next_state[3] = (in) & (state[2]);
+
+
+    assign out = state[3]; 
 
 
 endmodule 
